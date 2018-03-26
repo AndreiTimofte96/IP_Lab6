a67de56e6a56f7412c17538e14301e2644b36f68
Sergiu Iacob <sergiu.iacob1@gmail.com>
2018-03-26 15:08:04 +0300

    solution, solver

--- src/NSTSolver.java
+++ src/NSTSolver.java
@@ -1,4 +1,3 @@
-
 public class NSTSolver implements ISolver{
 
 	@Override
--- src/Solution.java
+++ src/Solution.java
@@ -1,4 +1,33 @@
-
 public class Solution {
+    private int nrPreferencesFulfilled;
+    Map <User, String> timetable;
 
+    Solution(){
+        nrPreferencesFulfilled = 0;
+        timetable = new HashMap<>();
+    }
+
+    public int getNrPreferencesFulfilled() {
+        return nrPreferencesFulfilled;
+    }
+
+    public void setNrPreferencesFulfilled(int nrPreferencesFulfilled) {
+        this.nrPreferencesFulfilled = nrPreferencesFulfilled;
+    }
+
+    public Map<User, String> getTimetable() {
+        return timetable;
+    }
+
+    public void setTimetable(Map<User, String> timetable) {
+        this.timetable = timetable;
+    }
+
+    @java.lang.Override
+    public java.lang.String toString() {
+        return "Solution{" +
+                "nrPreferencesFulfilled=" + nrPreferencesFulfilled +
+                ", timetable=" + timetable +
+                '}';
+    }
 }
