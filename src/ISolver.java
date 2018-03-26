a4f289cad2a2a7c51283f742cd7cdaab4e8c9875
Sergiu Iacob <sergiu.iacob1@gmail.com>
2018-03-26 15:28:16 +0300

    change from interface to abstract class

--- src/ISolver.java
+++ src/ISolver.java
@@ -1,4 +1,19 @@
-public interface ISolver {
+public abstract class ISolver {
     Solution solution;
-    void solve(List<Constraint> constraints);
+    void abstract solve(List<Constraint> constraints);
+
+    public Solution getSolution() {
+        return solution;
+    }
+
+    public void setSolution(Solution solution) {
+        this.solution = solution;
+    }
+
+    @java.lang.Override
+    public java.lang.String toString() {
+        return "ISolver{" +
+                "solution=" + solution +
+                '}';
+    }
 }
