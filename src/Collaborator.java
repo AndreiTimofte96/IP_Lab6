775229727007754beb2cf35f8b1def5319dffd65
Andrei <casuneanu.andrei97@gmail.com>
2018-03-26 15:07:36 +0300

    added some methods

--- src/Collaborator.java
+++ src/Collaborator.java
@@ -1,4 +1,11 @@
 
 public class Collaborator extends User implements ICanTeach{
+	public Collaborator(String name) {
+		super(name);
+	}
 
+	@Override
+	public void addPreference(String preference) {
+		preferences.add(preference);
+	}
 }
